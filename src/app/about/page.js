fa2684d6973de54f9ca0970ba15d61c647f60b4f
Romain Anezo <romainanezo@MacBook-Air-de-Romain.local>
2025-07-28 14:55:24 +0200

    Ajout de la page About

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>À propos de KissKash</h1>
+      <p>Nous permettons aux invités de contribuer facilement aux cagnottes de mariage.</p>
+    </main>
+  );
+}
